9412ad3a8919e4ba7d9acf7a650bc5459e9d6726
Dilan Ahmad <Dilan.Ahmad03@gmail.com>
2021-05-23 19:33:59 +0200

    Added some Basic Functions
    Cars will be initialized except the end Variable. I also added J-Unit Tests for the Cars Class

---
+++ Parkhaus-Simulation/src/main/java/parkhaus/Car.java
@@ -0,0 +1,66 @@
+package parkhaus;
+
+import java.util.Arrays;
+
+// ToDo replace 0 by correct values read from this.params
+public class Car implements CarIF {
+
+    private int nr;
+    private long begin;
+    private long end;
+    private int duration;
+    private int price;
+
+    public Car(int nr, long begin){
+        this.nr = nr;
+        this.begin = begin;
+        this.end = 0;
+        this.duration = 0;
+        this.price = 0;
+    }
+
+    @Override
+    public int getNr() {
+        return nr;
+    }
+
+    @Override
+    public long getBegin() {
+        return begin;
+    }
+
+    @Override
+    public long getEnd() {
+        return end;
+    }
+
+    @Override
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setEnd(long end) {
+        this.end = end;
+    }
+
+    @Override
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return  nr + ", " + begin + ", " + end + ", " + duration + ", " + price;
+    }
+}
